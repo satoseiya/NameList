f7c76c866fa7dc857b32be8bea250b317341d0eb
seiya <s.seiya1014@gmail.com>
2014-07-09 23:22:13 +0900

    modify welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :tops
+  root  'to_ps#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
